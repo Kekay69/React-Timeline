61817f96df21309c94a7476f9e5931d29e65923e
QuiQue697 <kekay69@hotmail.com>
2021-01-19 19:19:44 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './App.css';
+import { ReactComponent as WorkIcon } from "./img/work.svg";
+import { ReactComponent as SchoolIcon } from "./img/school.svg";
+
+import timelineElements from "./timelineElements";
+
+import {VerticalTimeline, VerticalTimelineElement} from "react-vertical-timeline-component";
+
+import "react-vertical-timeline-component/style.min.css";
+
+// class App extends Component {
+//   render() {
+function App() {
+  let workIconStyles = { background: "#06D6A0" };
+  let schoolIconStyles = { background: "#f9c74f" };
+
+    return ( 
+      <div className = "App">
+      <h1 className = "title">Timeline</h1> 
+        <VerticalTimeline>
+          {
+            timelineElements.map(element => {
+              let isWorkIcon = element.icon === "work";
+              let showButton = 
+                element.buttonText !== undefined &&
+                element.buttonText !== null && 
+                element.buttonText !== "";
+
+              return (
+                <VerticalTimelineElement
+                  key={element.key}
+                  date={element.date}
+                  dateClassName="date"
+                  iconStyle={isWorkIcon ? workIconStyles : schoolIconStyles}
+                  icon={isWorkIcon ? <WorkIcon /> : <SchoolIcon />}
+                >
+                  <h3 className="vertical-timeline-element-title">
+                  {element.title}
+                </h3>
+                <h5 className="vertical-timeline-element-subtitle">
+                  {element.location}
+                </h5>
+                <p id="description">{element.description}</p>
+                {showButton && (
+                  <a
+                    className={`button ${
+                      isWorkIcon ? "workButton" : "schoolButton"
+                    }`}
+                    href="/"
+                  >
+                    {element.buttonText}
+                  </a>
+                )}
+                </VerticalTimelineElement>
+              );
+            })
+          }
+        </VerticalTimeline>    
+      </div>
+    );
+}
+
+export default App;
---
+++ src/timelineElements.js
@@ -0,0 +1,74 @@
+let timelineElements = [
+    {
+      id: 1,
+      title: "Senior Consultant Developer",
+      location: "Remote",
+      description:
+        "Assist, Design, Debug, Redesign, and Test files using HTML5 CSS3, Bootstrap 4, Google Chrome, Firefox, and Edge for an Intranet Site.",
+      buttonText: "View: boundlessEnergy.net",
+      date: "2020/October - December",
+      icon: "work",
+    },
+    {
+      id: 2,
+      title: "Technology Lead - Web Developer ",
+      location: "Infosys/T-Mobile. Bellevue, WA",
+      description:
+        "Manual testing UI and its functionality accordingly to the Client Redlines with Angular, Bootstrap, HTML5, CSS3 including jQuery for Mobile, Tablets and Desktops. Manually testing Accessibility using only Jaws, per Client requirements.",
+      buttonText: "View Backend Projects",
+      date: "2015/December - 2019/December",
+      icon: "work",
+    },
+    {
+      id: 3,
+      title: "Web Developer",
+      location: "Synect Media. Redmond, WA",
+      description:
+        "Designing, and developing custom animations using HTML5 and CSS3 including jQuery and some JavaScript on digital signage for different customers. Last project designing the UI/UX Intranet website using Bootstrap framework for Synect Media customers.",
+      buttonText: "Intranet Company's Website",
+      date: "March - October 2015",
+      icon: "work",
+    },
+    {
+      id: 4,
+      title: "Web Developer",
+      location: "VMC/Microsoft. Redmond, WA",
+      description:
+        "Designing, building and managing responsive website for the Microsoft Games Studios Group. Worked within the development team to spec/design/support community feature efforts, across a range of platforms from .NET to Games for Windows Live and Xbox Live.",
+      buttonText: "Webs: Age of Empires, Rise of Nations, Castle Siege.",
+      date: "2013/August - 2014/October",
+      icon: "work",
+    },
+    {
+      id: 5,
+      title: "Bellevue College -Scrum Alliance.",
+      location: "Bellevue, WA",
+      description:
+        "Certified Scrum Master. Member ID: 1040754",
+      buttonText: "Course Certificate",
+      date: "2020/01",
+      icon: "school",
+    },
+    {
+      id: 6,
+      title: "Art Institute of Seattle",
+      location: "Seattle, WA",
+      description:
+        "Website Administration, with Multimedia Graphic Design.",
+      buttonText: "Associate in Applied Arts",
+      date: "1998/09 - 2002/03",
+      icon: "school",
+    },
+    {
+      id: 7,
+      title: "Facultad de Química",
+      location: "Universidad Nacional Autónoma de México",
+      description:
+        "Chemical Engineering",
+      buttonText: "Bachelor of Science Degree",
+      date: "1967/01 - 1973/03",
+      icon: "school",
+    },
+  ];
+  
+  export default timelineElements;
